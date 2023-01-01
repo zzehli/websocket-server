e58cc9682abdbbe316c6484d4185344ccdbd9764
jae <albertforus@gmail.com>
2023-01-01 00:08:23 -0500

    send message from server

--- server.mjs
+++ server.mjs
@@ -1,31 +1,71 @@
 import { createServer } from 'http';
 import crypto from 'crypto'
-import { encode } from 'punycode';
 const PORT = 8080;
 const MAGIC_STRING = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
 const SEVEN_BITS_PAYLOAD_SWITCH = 125
 const SIXTEEN_BITS_PAYLOAD_SWITCH  = 126
 const SIXTYFOUR_BITS_PAYLOAD_SWITCH = 127
 const MASKING_KEY_LEN_BYTES = 4
-//Bytes have 8 places, so the left-most place is 27 (or 128)
+//Bytes have 8 places, so the left-most place is 2 to the 7th power (or 128)
 const FIRST_BIT = 128
 
 const server = createServer((request, response) => {
     response.writeHead(200);
     response.end('hey there');
 })
-    .listen(8080, () => console.log('server listening to', PORT));
+    .listen(PORT, () => console.log('server listening to', PORT));
 
 server.on('upgrade', onSocketUpgrade);
 
 //establish handshake
 //refers to 4.2.1. Reading the Client's Opening Handshake
+//upgrade is a http client request: https://nodejs.org/api/http.html#event-upgrade
 function onSocketUpgrade(req, socket, head) {
     const { 'sec-websocket-key': webClientSocketKey } = req.headers
-    console.log(`${webClientSocketKey} connected`);
+    //console.log(`${webClientSocketKey} connected`);
     const headers = createHandShakeHeader(webClientSocketKey);
     socket.write(headers);
-    socket.on('readable', () => onSocketReadable(socket) )
+    socket.on('readable', () => onSocketReadable(socket))
+   
+}
+
+//send msg from server to frontend
+function sendMessage(msg, socket) {
+    const data = prepareMessage(msg);
+    socket.write(data);
+}
+
+function prepareMessage(message) {
+    const msg = Buffer.from(message);
+    //payload length
+    const messageSize = msg.length
+
+    let dataFrameBuffer;
+    let offset;
+
+    //first byte is text data (0x80 is 128 and 0x01 is OPCODE_TEXT)
+    const firstByte = 0x80 | 0x01 
+    if (messageSize <= SEVEN_BITS_PAYLOAD_SWITCH) {
+        const bytes = [firstByte];
+        dataFrameBuffer = Buffer.from(bytes.concat(messageSize));
+    } else {
+        throw new Error('message too long');
+    }
+
+    const totalLen = dataFrameBuffer.byteLength + messageSize;
+    const dataFrameResponse = concatBuffer([dataFrameBuffer, msg], totalLen);
+    return dataFrameResponse;
+}
+
+function concatBuffer(bufferList, totalLen) {
+    const target = Buffer.allocUnsafe(totalLen);
+    let offset = 0;
+    for(const buffer of bufferList) {
+        target.set(buffer, offset);
+        offset += buffer.length;
+    }
+
+    return target;
 }
 
 //read payload length, refer to 5.2. Base Framing Protocol
@@ -48,17 +88,25 @@ function onSocketReadable(socket) {
     //encoded is a buffer, encoded by masking key
     const encoded = socket.read(messageLength);
     const decoded = unmask(encoded, maskingKey);
-    console.log(decoded.toString())
+    //console.log(decoded.toString())
+    const decodedData = JSON.parse(decoded.toString('utf8'));
+    console.log(decodedData)
+
+    const msg = JSON.stringify({
+        message: decodedData,
+        at: new Date().toISOString()
+    })
+    sendMessage(msg, socket);
 }
 
 //unmask encoded data from client, refer to 5.3Client-to-Server Masking
 function unmask(encodedBuffer, maskingKey) {
     //https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers
     // Create the byte Array of decoded payload
-    //const copyBuffer = Buffer.from(encodedBuffer);
 
     const final = Uint8Array.from(encodedBuffer, (elt, i) => elt ^ maskingKey[i % 4]); // Perform an XOR on the mask
     return Buffer.from(final)
+    //alternative: 
     // const finalBuffer = Buffer.from(encodedBuffer);
     // for (let index = 0; index < encodedBuffer.length; index++) {
     //     finalBuffer[index] = encodedBuffer[index] ^ maskingKey[index % 4];
