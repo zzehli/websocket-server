3a4c0595167173c2c5ec466438fab94d03a0b0c8
jae <albertforus@gmail.com>
2023-01-01 14:34:48 -0500

    change frontend, update README

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# WebSocket Server
-# Reference
-* https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers#miscellaneous
+# Websocket Server
+This is a barebone websocket server built according to [RFC 6455](https://www.rfc-editor.org/rfc/rfc6455.html). The server has two main components: making the handshake (upgrade from HTTP to WebSocket protocol) and handling data frames (how messages are transmitted through the protocol). The server handles messages up to 16 bits.
+
+[Demo](https://youtu.be/dVPiCg_Tp90)
+
+# References
+
+* Dion Misic: A Beginner's Guide to WebSockets https://www.youtube.com/watch?v=qFoFKLI3O8w
+* MDN: Writing Websocket Servers https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers#miscellaneous
+* Erick Wendel: Building a websocket app from scratch https://www.youtube.com/watch?v=PjiXkJ6P9pQ
+* RFC 6455: The WebSocket Protocol https://www.rfc-editor.org/rfc/rfc6455.html
--- server.mjs
+++ server.mjs
@@ -31,12 +31,73 @@ function onSocketUpgrade(req, socket, head) {
    
 }
 
+//create the handshake header, including the acceptkey according to 1.3 Opening Handshake
+function createHandShakeHeader(id) {
+    const acceptKey = createSocketAccept(id)
+    const headers = [
+        'HTTP/1.1 101 Switching Protocols',
+        'Upgrade: websocket',
+        'Connection: Upgrade',
+        `Sec-WebSocket-Accept: ${acceptKey}`,
+        ''
+    ].map(elem => elem.concat('\r\n')).join('');
+    return headers;
+}
+
+function createSocketAccept(id) {
+    const hash = crypto.createHash('sha1')
+    hash.update(id + MAGIC_STRING)
+    return hash.digest('base64')
+}
+
+//read payload length, refer to 5.2. Base Framing Protocol
+function onSocketReadable(socket) {
+    //consume optcode (first byte)
+    socket.read(1);
+
+    //check payload len, starting after the MASK bit
+    const [markerPayloadLen] = socket.read(1);
+    const payloadIndicator = markerPayloadLen - FIRST_BIT;
+
+    let messageLength = 0;
+    if(payloadIndicator <= SEVEN_BITS_PAYLOAD_SWITCH){
+        messageLength = payloadIndicator;
+    } else if (payloadIndicator == SIXTEEN_BITS_PAYLOAD_SWITCH) {
+        messageLength = socket.read(2).readUint16BE(0);
+    } else {
+        throw new Error('your message is too long!')
+    }
+
+    const maskingKey = socket.read(MASKING_KEY_LEN_BYTES);
+    //encoded is a buffer, encoded by masking key
+    const encoded = socket.read(messageLength);
+    let msg = unmask(encoded, maskingKey);
+    console.log('received: ' + msg.toString());
+    msg = msg + ' uuid: ' + crypto.randomUUID();
+    sendMessage(msg, socket);
+}
+
+//unmask encoded data from client, refer to 5.3Client-to-Server Masking
+function unmask(encodedBuffer, maskingKey) {
+    //https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers
+    // Create the byte Array of decoded payload
+    //alternative: 
+    // const final = Uint8Array.from(encodedBuffer, (elt, i) => elt ^ maskingKey[i % 4]); // Perform an XOR on the mask
+    // return Buffer.from(final)
+    const finalBuffer = Buffer.from(encodedBuffer);
+    for (let index = 0; index < encodedBuffer.length; index++) {
+        finalBuffer[index] = encodedBuffer[index] ^ maskingKey[index % 4];
+    }
+    return finalBuffer
+}
+
 //send msg from server to frontend
 function sendMessage(msg, socket) {
     const data = prepareMessage(msg);
     socket.write(data);
 }
 
+//prepare the message according to framing specified in 5.2. Base Framing Protocol
 function prepareMessage(message) {
     const msg = Buffer.from(message);
     //payload length
@@ -77,72 +138,6 @@ function concatBuffer(bufferList, totalLen) {
     }
 
     return target;
-}
-
-//read payload length, refer to 5.2. Base Framing Protocol
-function onSocketReadable(socket) {
-    //consume optcode (first byte)
-    socket.read(1);
-
-    //check payload len, starting after the MASK bit
-    const [markerPayloadLen] = socket.read(1);
-    const payloadIndicator = markerPayloadLen - FIRST_BIT;
-
-    let messageLength = 0;
-    if(payloadIndicator <= SEVEN_BITS_PAYLOAD_SWITCH){
-        messageLength = payloadIndicator;
-    } else if (payloadIndicator == SIXTEEN_BITS_PAYLOAD_SWITCH) {
-        messageLength = socket.read(2).readUint16BE(0);
-    } else {
-        throw new Error('your message is too long!')
-    }
-
-    const maskingKey = socket.read(MASKING_KEY_LEN_BYTES);
-    //encoded is a buffer, encoded by masking key
-    const encoded = socket.read(messageLength);
-    const decoded = unmask(encoded, maskingKey);
-    //console.log(decoded.toString())
-    const decodedData = JSON.parse(decoded.toString('utf8'));
-    console.log(decodedData)
-
-    const msg = JSON.stringify({
-        message: decodedData,
-        at: new Date().toISOString()
-    })
-    sendMessage(msg, socket);
-}
-
-//unmask encoded data from client, refer to 5.3Client-to-Server Masking
-function unmask(encodedBuffer, maskingKey) {
-    //https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers
-    // Create the byte Array of decoded payload
-    //alternative: 
-
-    // const final = Uint8Array.from(encodedBuffer, (elt, i) => elt ^ maskingKey[i % 4]); // Perform an XOR on the mask
-    // return Buffer.from(final)
-    const finalBuffer = Buffer.from(encodedBuffer);
-    for (let index = 0; index < encodedBuffer.length; index++) {
-        finalBuffer[index] = encodedBuffer[index] ^ maskingKey[index % 4];
-    }
-    return finalBuffer
-}
-
-function createHandShakeHeader(id) {
-    const acceptKey = createSocketAccept(id)
-    const headers = [
-        'HTTP/1.1 101 Switching Protocols',
-        'Upgrade: websocket',
-        'Connection: Upgrade',
-        `Sec-WebSocket-Accept: ${acceptKey}`,
-        ''
-    ].map(elem => elem.concat('\r\n')).join('');
-    return headers;
-}
-
-function createSocketAccept(id) {
-    const hash = crypto.createHash('sha1')
-    hash.update(id + MAGIC_STRING)
-    return hash.digest('base64')
 }
 
 //error handling
